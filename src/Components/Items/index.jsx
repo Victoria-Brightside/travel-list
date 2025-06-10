191e11ad3849cea32039ed5905055f4bfb2b6665
Seier <seier.bernal@gmail.com>
2025-06-10 16:54:47 -0600

    Se agrega lógica js para funcionalidad de agregar items a la lista, se agrega función map para renderizar los items agregados a la lista

--- src/Components/Items/index.jsx
+++ src/Components/Items/index.jsx
@@ -1,23 +1,49 @@
 import "./Items.css"
+import { useState } from "react";
 
 function Items() {
+    const [num, setNum] = useState("");
+    const [item, setItem] = useState("");
+    const [arrayItems, setArrayItems] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!num || !item) return;
+        setArrayItems(listaPrevia => [...listaPrevia, { num, item }]);
+        console.log(num, item);
+        setNum("");
+        setItem("");
+    }
+
 
     return (
         <>
             <section className="getInfo_container">
                 <h2 className="getInfo_question">¿Qué necesitas para tu viaje?</h2>
-                <input className="getInfo_number" type="number" />
-                <input className="getInfo_text" type="text" placeholder="Pasaporte.." />
-                <button className="getInfo_btn"> Agregar </button>
+                <form onSubmit={handleSubmit} className="getInfo_form">
+                    <input
+                        className="getInfo_number"
+                        type="number"
+                        value={num}
+                        onChange={(e) => setNum(e.target.value)}
+                    />
+                    <input
+                        className="getInfo_text"
+                        type="text"
+                        value={item}
+                        onChange={(e) => setItem(e.target.value)}
+                        placeholder="Pasaporte.." />
+                    <button className="getInfo_btn" type="submit"> Agregar </button>
+                </form>
             </section>
 
             <section className="items_container">
-                <div className="single_item" >
-                    <input className="check_item" type="checkbox" />
-                    <h3> Nombre item </h3>
-                </div>
-
-
+                {arrayItems.map((obj, index) => (
+                    <div className="single_item" key={index}>
+                        <input className="check_item" type="checkbox" />
+                        <h3> {obj.num} {obj.item} </h3>
+                    </div>
+                ))}
             </section>
         </>
     )
