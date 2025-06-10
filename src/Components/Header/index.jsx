9e4609fcc96cefe7e7ad6f37265472dd37d5a2cf
Seier <seier.bernal@gmail.com>
2025-06-10 12:02:33 -0600

    Se crea componente header

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function Header() {
+    return (
+        <div className="header_container">
+            <img className="header_img" src="/mapa_mundo.jpg" alt="Imágen de globo terraqueo con fondo oscuro"/>
+            <h1 className="header_title"> Wanderlust</h1>
+        </div>
+    )
+
+}
+
+export default Header;
